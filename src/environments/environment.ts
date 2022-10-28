3c722cd75d7a09db91c5051c945227700184b781
agab0110 <agabrielex01@gmail.com>
2022-10-28 18:18:13 +0200

    28/10/2022

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'project-87ed6',
+    appId: '1:267554896882:web:f6d113155707af2c9b096b',
+    databaseURL: 'https://project-87ed6-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'project-87ed6.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCpFpCovtCMkm53umcoWrCDtNNwDTdjIWw',
+    authDomain: 'project-87ed6.firebaseapp.com',
+    messagingSenderId: '267554896882',
+  },
   production: false
 };
 
