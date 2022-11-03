8a393f8f710eeae750a4279349f8bec2432f429f
agab0110 <agabrielex01@gmail.com>
2022-11-03 11:28:10 +0100

    03/11/2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,10 +21,6 @@ const routes: Routes = [
     redirectTo: '',
     pathMatch: 'full'
   },
-  {
-    path: 'registration',
-    loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
-  },
 ];
 
 @NgModule({
