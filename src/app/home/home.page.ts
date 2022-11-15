388a196e5ce65d2f0c2ef4010ab6fb639f080b9e
agab0110 <agabrielex01@gmail.com>
2022-11-15 09:37:24 +0100

    15/11/2022

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -33,8 +33,8 @@ export class HomePage {
     await loading.present();
 
     PSPDFKit.load({
-      container: '',
       document: documentUrl,
+      container: documentUrl
     });
 
     await loading.dismiss();
